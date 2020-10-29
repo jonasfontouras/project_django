058b0e6e51ad46174a387145b02e7662ca3b0219
jonasfontouras <jonasfontouras@gmail.com>
2020-10-28 23:25:16 -0300

    Configuração da página de admin, inclusão de cursos p/ teste, filtros de pesquina no django admin

--- projeto_django/settings.py
+++ projeto_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'courses',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'projeto_django', 'media')
